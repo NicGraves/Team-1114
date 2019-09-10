2e17baf290d45147c5d02da90b15465b05774c3b
NicG <nlgraves92@gmail.com>
2019-09-10 15:27:38 -0500

    space

--- SDPRO/src/Main.java
+++ SDPRO/src/Main.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
