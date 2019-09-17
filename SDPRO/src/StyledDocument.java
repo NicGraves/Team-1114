5a87aabdc869f18ed6825b59761715ffbed193b3
Duncan McPhie <47937277+DuncanMcPhie@users.noreply.github.com>
2019-09-17 15:02:18 -0500

    Merged Josh and Jareds work.
    Merged Josh and Jared branch.
    Made jpane work.
    Made color work.

--- SDPRO/src/StyledDocument.java
+++ SDPRO/src/StyledDocument.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ProjectPackage;
 
 import java.awt.Color;
 import javax.swing.text.AttributeSet;
--- SDPRO/src/textEditor.java
+++ SDPRO/src/textEditor.java
@@ -1,7 +1,12 @@
 import java.awt.BorderLayout;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
@@ -9,10 +14,34 @@ import javax.swing.border.TitledBorder;
 public class textEditor 
 {
 	private JPanel textEditor = new JPanel(new BorderLayout()); //create a new JPanel with a border layout
-	private JTextArea ta = new JTextArea(); //Create a new JTextArea
+	private JTextPane ta; //Create a new JTextPane
 	
 	public void buildTextEditor()
 	{
+      String redKeywords = "";
+      String blueKeywords = "";
+      FileReader in;
+      try
+      {
+          in = new FileReader("Keywords.txt");
+          int character;
+          while ((character = in.read()) != 10 && character != -1)
+          {
+              blueKeywords += (char)character;
+          }
+          while ((character = in.read()) != 10 && character != -1)
+          {
+              redKeywords += (char)character;
+          }
+          in.close();
+      }
+      catch(IOException e)
+      {
+          System.out.println(e);
+          System.exit(-1);
+      }
+      StyledDocument doc = new StyledDocument(blueKeywords, redKeywords);
+      ta = new JTextPane(doc);
 		textEditor.setBorder ( new TitledBorder ( new EtchedBorder (), "Text Editor" ) ); //create a border around the JPanel with the name "Text Editor"
         JScrollPane scroll = new JScrollPane ( ta ); //Create a new JScrollPane and add the JTextArea
 	    scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED ); //Set the scroll bar to only appear when necessary
@@ -24,7 +53,7 @@ public class textEditor
 		return textEditor;
 	}
 	
-	protected JTextArea getTextArea()
+	protected JTextPane getTextArea()
 	{
 		return ta;
 	}
