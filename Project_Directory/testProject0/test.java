1730af91f4a7b86161cc906bbf3447d00c51c637
Duncan McPhie <47937277+DuncanMcPhie@users.noreply.github.com>
2019-10-08 13:45:11 -0500

    Testing the class folder.

--- Project_Directory/testProject0/test.java
+++ Project_Directory/testProject0/test.java
@@ -2,8 +2,8 @@ public class test
 {
 	public static void main(String[] args)
 	{
-//		test2 t = new test2();
-//		t.print("GOT IT");
+		test2 t = new test2();
+		t.print("GOT IT");
 		System.out.println("Hello World");
 	}
 	
