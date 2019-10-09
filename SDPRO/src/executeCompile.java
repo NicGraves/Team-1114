d41b01cb46b74bff0ce0fe1d1488bfe27b9646ef
Duncan McPhie <47937277+DuncanMcPhie@users.noreply.github.com>
2019-10-09 14:09:34 -0500

    Working with compile/execute.

--- SDPRO/src/executeCompile.java
+++ SDPRO/src/executeCompile.java
@@ -3,6 +3,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.BufferedOutputStream; 
+import java.io.OutputStream;
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
 
 public class executeCompile 
 {
@@ -13,11 +17,11 @@ public class executeCompile
 	{
 		if(currentProject.length() != 0 && currentFile.length() != 0)
 		{
-	    	runProcess("javac -d Class "+getFiles(currentProject));
+	    	runProcess(getFiles(currentProject));
 	    	if(output.equals(""))
 	    	{
 	    		String runFile = currentFile.toString().substring(0, currentFile.toString().indexOf("."));
-				runProcess("java -cp Class "+runFile);
+				runProcess(runFile);
 	    	}
 		}
 		else if(currentProject.length() == 0)
@@ -32,26 +36,35 @@ public class executeCompile
 		return output;
 	}
 	
-	private void printLines(InputStream ins) throws IOException
-	{
-		String line = null;
-	    BufferedReader in = new BufferedReader(new InputStreamReader(ins));
-	    while ((line = in.readLine()) != null) 
-	    {
-	    	output += line+"\n";
-	    }
-	}
+//	private void printLines(InputStream ins) throws IOException
+//	{
+//		String line = null;
+//	    BufferedReader in = new BufferedReader(new InputStreamReader(ins));
+//	    while ((line = in.readLine()) != null) 
+//	    {
+//	    	output += line+"\n";
+//	    }
+//	}
 
 	private void runProcess(String command) throws IOException, InterruptedException
 	{
-		Process pro = Runtime.getRuntime().exec(command);
 		output = "";
-		printLines(pro.getErrorStream());
-		if(output.equals(""))
-		{
-			printLines(pro.getInputStream());
-		}
+		ProcessBuilder builder = new ProcessBuilder("java", command);
+		builder.redirectError();
+		builder.directory(new File("src"));
+		Process pro = builder.start();
+		InputStreamConsumer ins = new InputStreamConsumer(pro.getErrorStream(), pro.getInputStream(), output);
+		ins.start();
 		pro.waitFor();
+		ins.join();
+		output = ins.getOutput();
+
+//		output = "";
+//		printLines(pro.getErrorStream());
+//		if(output.equals(""))
+//		{
+//			printLines(pro.getInputStream());
+//		}
 	}
 	
 	private String getFiles(StringBuilder currentProject)
@@ -66,4 +79,64 @@ public class executeCompile
 		}
 		return fileNames;
 	}
+	
+	public class InputStreamConsumer extends Thread 
+	{
+		private InputStream es;
+        private InputStream is;
+        private IOException exp;
+        private String output;
+
+        public InputStreamConsumer(InputStream es, InputStream is, String output) 
+        {
+        	this.es = es;
+            this.is = is;
+            this.output = output;
+        }
+
+        @Override
+        public void run()
+        {
+	    		String line = null;
+	    	    BufferedReader in = new BufferedReader(new InputStreamReader(es));
+	    	    try 
+	    	    {
+					while ((line = in.readLine()) != null) 
+					{
+						output += line+"\n";
+					}
+				} 
+	    	    catch (IOException e) 
+	    	    {
+					e.printStackTrace();
+				}
+	    	    if(output.equals(""))
+	    	    {
+	        		line = null;
+	        	    BufferedReader in2 = new BufferedReader(new InputStreamReader(is));
+	        	    try 
+	        	    {
+						while ((line = in2.readLine()) != null) 
+						{
+							output += line+"\n";
+						}
+					} 
+	        	    catch (IOException e) 
+	        	    {
+						e.printStackTrace();
+					}
+        	
+	    	    }
+	    	    
+        }
+        public String getOutput() 
+        {
+            return output;
+        }
+
+        public IOException getException() 
+        {
+            return exp;
+        }
+    }
 }
