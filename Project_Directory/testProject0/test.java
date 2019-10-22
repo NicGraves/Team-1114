b7ef217d34870ed7473dd8648cd43cd9b64f757e
JKaitoK <joshuakrchnak@gmail.com>
2019-10-22 13:23:23 -0500

    Testing
    Did some testing on the files

--- Project_Directory/testProject0/test.java
+++ Project_Directory/testProject0/test.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;  // Import the Scanner class
 public class test 
 {
 	public static void main(String[] args)
 	{
-//		test2 t = new test2();
-//		t.print("GOT IT");
+		test2 t = new test2();
+		t.print("GOT IT");
 		System.out.println("Hello World");
+		//Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+  		//System.out.println("Enter username");
+ 		//String userName = myObj.nextLine();  // Read user input
+    		//System.out.println("Username is: " + userName);  // Output user input
 	}
 }
