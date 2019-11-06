2bea98d79518321539a3a4574bf7ec2a28fc9464
JKaitoK <joshuakrchnak@gmail.com>
2019-11-05 20:03:46 -0600

    ClassLoader
    Working on compiling using class loader

--- SDPRO/src/CCLRun.java
+++ SDPRO/src/CCLRun.java
@@ -18,17 +18,17 @@ public class CCLRun
 		// Create a CompilingClassLoader
 		CompilingClassLoader ccl = new CompilingClassLoader();
 		// Load the main class through our CCL
-		Class clas = ccl.loadClass( progClass );
+		Class<?> clas = ccl.loadClass( progClass );
 		// Use reflection to call its main() method, and to
 		// pass the arguments in.
 		// Get a class representing the type of the main method's argument
-		Class mainArgType[] = { (new String[0]).getClass() };
-		// Find the standard main method in the class
+		Class<?> mainArgType[] = { (new String[0]).getClass() };
+		//Find the standard main method in the class
 		Method main = clas.getMethod( "main", mainArgType );
-		// Create a list containing the arguments -- in this case,
-		// an array of strings
+		//Create a list containing the arguments -- in this case,
+		//an array of strings
 		Object argsArray[] = { progArgs };
-		// Call the method
+		//Call the method
 		main.invoke( null, argsArray );
 	}
 }
--- SDPRO/src/CompilingClassLoader.java
+++ SDPRO/src/CompilingClassLoader.java
@@ -22,15 +22,12 @@ public class CompilingClassLoader extends ClassLoader
 		// then it's an error.
 		int r = fin.read( raw );
 		if (r != len)
-		{
-			throw new IOException( "Can't read all, "+r+" != "+len );
-		}
+		throw new IOException( "Can't read all, "+r+" != "+len );
 		// Don't forget to close the file!
 		fin.close();
 		// And finally return the file contents as an array
 		return raw;
 	}
-	
 	// Spawn a process to compile the java source code file
 	// specified in the 'javaFile' parameter. Return a true if
 	// the compilation worked, false otherwise.
@@ -40,70 +37,76 @@ public class CompilingClassLoader extends ClassLoader
 		System.out.println( "CCL: Compiling "+javaFile+"..." );
 		// Start up the compiler
 		Process p = Runtime.getRuntime().exec( "javac "+javaFile );
-		// Wait for it to finish running
-		try {
-		p.waitFor();
-		} catch( InterruptedException ie ) { System.out.println( ie ); }
-		// Check the return code, in case of a compilation error
+		//Wait for it to finish running
+		try 
+		{
+			p.waitFor();
+		} 
+		catch( InterruptedException ie ) 
+		{ 
+			System.out.println( ie ); 
+		}
+		//Check the return code, in case of a compilation error
 		int ret = p.exitValue();
-		// Tell whether the compilation worked
+		//Tell whether the compilation worked
 		return ret==0;
 	}
-	// The heart of the ClassLoader -- automatically compile
-	// source as necessary when looking for class files
+	//The heart of the ClassLoader -- automatically compile
+	//source as necessary when looking for class files
 	public Class loadClass( String name, boolean resolve ) throws ClassNotFoundException 
 	{
-		// Our goal is to get a Class object
+		//Our goal is to get a Class object
 		Class clas = null;
-		// First, see if we've already dealt with this one
+		//First, see if we've already dealt with this one
 		clas = findLoadedClass( name );
 		//System.out.println( "findLoadedClass: "+clas );
-		// Create a pathname from the class name
-		// E.g. java.lang.Object => java/lang/Object
+		//Create a pathname from the class name
+		//E.g. java.lang.Object => java/lang/Object
 		String fileStub = name.replace( '.', '/' );
-		// Build objects pointing to the source code (.java) and object
-		// code (.class)
+		//Build objects pointing to the source code (.java) and object
+		//code (.class)
 		String javaFilename = fileStub+".java";
 		String classFilename = fileStub+".class";
 		File javaFile = new File( javaFilename );
 		File classFile = new File( classFilename );
 		//System.out.println( "j "+javaFile.lastModified()+" c "+
-		// classFile.lastModified() );
-		// First, see if we want to try compiling. We do if (a) there
-		// is source code, and either (b0) there is no object code,
-		// or (b1) there is object code, but it's older than the source
-		if (javaFile.exists() && (!classFile.exists() || javaFile.lastModified() > classFile.lastModified())) 
+		//classFile.lastModified() );
+		//First, see if we want to try compiling. We do if (a) there
+		//is source code, and either (b0) there is no object code,
+		//or (b1) there is object code, but it's older than the source
+		if (javaFile.exists() &&
+		(!classFile.exists() ||
+		javaFile.lastModified() > classFile.lastModified())) 
 		{
 			try 
 			{
-				// Try to compile it. If this doesn't work, then
-				// we must declare failure. (It's not good enough to use
-				// and already-existing, but out-of-date, classfile)
+				//Try to compile it. If this doesn't work, then
+				//we must declare failure. (It's not good enough to use
+				//and already-existing, but out-of-date, classfile)
 				if (!compile( javaFilename ) || !classFile.exists()) 
 				{
 					throw new ClassNotFoundException( "Compile failed: "+javaFilename );
 				}
-			} 
-			catch( IOException ie ) 
+			} catch( IOException ie ) 
 			{
-				// Another place where we might come to if we fail
-				// to compile
+				//Another place where we might come to if we fail
+				//to compile
 				throw new ClassNotFoundException( ie.toString() );
 			}
 		}
-		// Let's try to load up the raw bytes, assuming they were
-		// properly compiled, or didn't need to be compiled
+		//Let's try to load up the raw bytes, assuming they were
+		//properly compiled, or didn't need to be compiled
 		try 
 		{
-			// read the bytes
+			//read the bytes
 			byte raw[] = getBytes( classFilename );
-			// try to turn them into a class
+			//try to turn them into a class
 			clas = defineClass( name, raw, 0, raw.length );
 		} 
 		catch( IOException ie ) 
 		{
-			// This is not a failure! If we reach here, it might
-			// mean that we are dealing with a class in a library,
+			//This is not a failure! If we reach here, it might
+			//mean that we are dealing with a class in a library,
 			// such as java.lang.Object
 		}
 		//System.out.println( "defineClass: "+clas );
